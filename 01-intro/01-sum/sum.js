949ec6b1b204484ecf00ee5184fdaaf0ff4804ee
Repkin Vasiliy <vasiliy-mobile@yandex.ru>
2022-04-21 22:10:30 +0200

    Task 01
    Implement sum function

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,13 @@
 function sum(a, b) {
-  /* ваш код */
+  if ('number' !== typeof a || 'number' !== typeof b) {
+    throw new TypeError('Type of arguments not is number');
+  }
+
+  if (!Number.isFinite(a) || !Number.isFinite(b)) {
+    throw new RangeError('Value of arguments out of range');
+  }
+
+  return a + b;
 }
 
 module.exports = sum;
