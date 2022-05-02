373f0e73441c6e48301a49d4b0dce7a0c1abc273
Repkin Vasiliy <vasiliy-mobile@yandex.ru>
2022-05-02 12:11:00 +0200

    Task 03 (01)
    Implement limit size stream

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -4,10 +4,20 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+
+    this.limit = options.limit || 0;
+    this.totalSize = 0;
   }
 
   _transform(chunk, encoding, callback) {
+    this.totalSize += Buffer.byteLength(chunk);
+    let error = null;
 
+    if (this.limit < this.totalSize) {
+      error = new LimitExceededError();
+    }
+
+    callback(error, chunk);
   }
 }
 
