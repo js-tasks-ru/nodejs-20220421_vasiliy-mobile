5803d033df1f100b0c4ba3f075a55e3fe34419c2
Repkin Vasiliy <vasiliy-mobile@yandex.ru>
2022-05-11 16:29:57 +0200

    Task 03 [02]
    Implement line split stream

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -4,12 +4,37 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+
+    this.lastLine = '';
   }
 
   _transform(chunk, encoding, callback) {
+    const lines = chunk.toString().split(os.EOL);
+    const lastLineIndex = lines.length - 1;
+
+    lines.forEach((line, index) => {
+      if (0 === index && this.lastLine) {
+        this.push(this.lastLine + line);
+        this.lastLine = '';
+
+        return true; // continue
+      }
+
+      if (index < lastLineIndex) {
+        this.push(line);
+      } else {
+        this.lastLine = line;
+      }
+    });
+
+    callback(null);
   }
 
   _flush(callback) {
+    const line = this.lastLine;
+    this.lastLine = '';
+
+    callback(null, line);
   }
 }
 
