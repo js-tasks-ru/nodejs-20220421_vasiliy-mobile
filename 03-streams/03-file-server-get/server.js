09cd70f45f862c876ad28b1d0c31f6f6eb2bcb4e
Repkin Vasiliy <vasiliy-mobile@yandex.ru>
2022-05-12 16:19:49 +0200

    Task 03 [03]
    Implement file loading method

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -1,6 +1,8 @@
-const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
+
+const isPathOfFirstLevel = require('./isPathOfFirstLevel');
 
 const server = new http.Server();
 
@@ -12,6 +14,30 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'GET':
+      if (!isPathOfFirstLevel(pathname)) {
+        res.statusCode = 400;
+        res.end('Included path not supported');
+
+        return;
+      }
+
+      const fileStream = fs.createReadStream(filepath);
+
+      fileStream.pipe(res);
+
+      fileStream.on('error', (err) => {
+        if ('ENOENT' === err.code) {
+          res.statusCode = 404;
+          res.end('File not exists');
+        } else {
+          res.statusCode = 500;
+          res.end('Internal error');
+        }
+      });
+
+      req.on('aborted', () => {
+        fileStream.destroy();
+      });
 
       break;
 
