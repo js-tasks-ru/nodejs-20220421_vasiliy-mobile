ad39823bff38b8df6298f57c7b6a24756a3c06c1
Repkin Vasiliy <vasiliy-mobile@yandex.ru>
2022-05-13 20:33:54 +0200

    Task 03 [04]
    Implement file saving

--- 03-streams/04-file-server-post/server.js
+++ 03-streams/04-file-server-post/server.js
@@ -1,17 +1,60 @@
-const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
+
+const saveFile = require('./saveFile');
+
+const FILES_ROOT = path.join(__dirname, 'files');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
-  const pathname = url.pathname.slice(1);
+  let pathname = url.pathname.slice(1);
 
-  const filepath = path.join(__dirname, 'files', pathname);
+  try {
+    pathname = decodeURIComponent(pathname);
+  } catch (e) {
+    res.statusCode = 400;
+    res.end('Path not supported');
+
+    return;
+  }
+
+  if (-1 !== pathname.indexOf('\0')) {
+    res.statusCode = 400;
+    res.end('Path not supported');
+
+    return;
+  }
+
+  const filepath = path.join(FILES_ROOT, pathname);
 
   switch (req.method) {
     case 'POST':
+      if (!pathname || /[\/\\]/.test(pathname)) {
+        res.statusCode = 400;
+        res.end('Path not supported');
+
+        return;
+      }
+
+      fs.stat(filepath, (err) => {
+        if (err) {
+          if ('ENOENT' === err.code) {
+            saveFile(filepath, req, res);
+          } else {
+            res.statusCode = 500;
+            res.end('Internal error');
+          }
+
+          return;
+        }
+
+
+        res.statusCode = 409;
+        res.end('File already exists');
+      });
 
       break;
 
