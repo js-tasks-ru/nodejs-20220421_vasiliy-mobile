b380f8e7db0ce6dbeef2e90fb5e81d1e9dda853e
Repkin Vasiliy <vasiliy-mobile@yandex.ru>
2022-05-26 23:39:34 +0200

    Task 04 [01]
    Create tests for checking type of value and rule config

--- 04-testing-configuration-logging/01-unit-tests/test/Validator.test.js
+++ 04-testing-configuration-logging/01-unit-tests/test/Validator.test.js
@@ -1,9 +1,51 @@
 const Validator = require('../Validator');
 const expect = require('chai').expect;
 
-describe('testing-configuration-logging/unit-tests', () => {
+describe.only('testing-configuration-logging/unit-tests', () => {
   describe('Validator', () => {
-    it('валидатор проверяет строковые поля', () => {
+    it('валидация: передан неверный тип', () => {
+      const validator = new Validator({
+        name: {
+          type: 'string',
+          min: 3,
+          max: 10,
+        },
+        age: {
+          type: 'number',
+          min: 18,
+          max: 100,
+        },
+      });
+
+      const errors = validator.validate({name: 20, age: 'eighteen'});
+
+      expect(errors).to.have.length(2);
+
+      expect(errors[0]).to.have.property('field').and.to.be.equal('name');
+      expect(errors[0]).to.have.property('error').and.to.be.equal('expect string, got number');
+
+      expect(errors[1]).to.have.property('field').and.to.be.equal('age');
+      expect(errors[1]).to.have.property('error').and.to.be.equal('expect number, got string');
+    });
+
+    it('валидация: минимальное значение больше максимального', () => {
+      const validator = new Validator({
+        name: {
+          type: 'string',
+          min: 10,
+          max: 3,
+        },
+      });
+
+      const errors = validator.validate({name: 'username'});
+
+      expect(errors[0]).to.have.property('field').and.to.be.equal('name');
+      expect(errors[0]).to.have.property('error').and.to.be.equal('max is less than min');
+    });
+  });
+
+  describe('Validator of strings', () => {
+    it('валидация строки: строка слишком короткая', () => {
       const validator = new Validator({
         name: {
           type: 'string',
@@ -12,7 +54,7 @@ describe('testing-configuration-logging/unit-tests', () => {
         },
       });
 
-      const errors = validator.validate({ name: 'Lalala' });
+      const errors = validator.validate({name: 'Lalala'});
 
       expect(errors).to.have.length(1);
       expect(errors[0]).to.have.property('field').and.to.be.equal('name');
