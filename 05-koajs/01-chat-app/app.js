eab58d8b3dd9b749e56e9191368e2bd91e7a5a04
Repkin Vasiliy <vasiliy-mobile@yandex.ru>
2022-06-03 02:09:28 +0200

    Task 05 [01]
    Create chat width long polling

--- 05-koajs/01-chat-app/app.js
+++ 05-koajs/01-chat-app/app.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const uuid = require('uuid');
 const Koa = require('koa');
 const app = new Koa();
 
@@ -8,10 +9,38 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
-router.get('/subscribe', async (ctx, next) => {
+const sessions = Object.create(null);
+
+router.get('/subscribe', async (ctx) => {
+  const sessionId = uuid.v4();
+
+  ctx.req.on('aborted', () => {
+    delete sessions[sessionId];
+  });
+
+  return await new Promise((resolve) => {
+    sessions[sessionId] = resolve.bind(this);
+  }).then((message) => {
+    ctx.body = message;
+  }).finally(() => {
+    delete sessions[sessionId];
+  });
 });
 
-router.post('/publish', async (ctx, next) => {
+router.post('/publish', async (ctx) => {
+  const {message} = ctx.request.body;
+
+  if (!message) {
+    ctx.status = 400;
+
+    return;
+  }
+
+  for (const [, resolve] of Object.entries(sessions)) {
+    resolve(message);
+  }
+
+  ctx.body = 'OK';
 });
 
 app.use(router.routes());
