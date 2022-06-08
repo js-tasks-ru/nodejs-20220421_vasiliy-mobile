6ee4e4c67ea1fd7a88b88d66494791589868ff40
Repkin Vasiliy <vasiliy-mobile@yandex.ru>
2022-06-08 17:20:51 +0200

    Task 06 [02]
    Implement product controller: get product by id

--- 06-mongodb-mongoose/02-rest-api/controllers/products.js
+++ 06-mongodb-mongoose/02-rest-api/controllers/products.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const Product = require('./../models/Product');
 const productMapper = require('./../mappers/product');
 
@@ -18,6 +19,19 @@ module.exports.productList = async function productList(ctx, next) {
 };
 
 module.exports.productById = async function productById(ctx, next) {
-  ctx.body = {};
-};
+  const productId = ctx.params.id;
 
+  if (!mongoose.isValidObjectId(productId)) {
+    ctx.status = 400;
+
+    return;
+  }
+
+  const product = await Product.findById(productId);
+
+  if (product) {
+    ctx.body = {product: productMapper(product)};
+  } else {
+    ctx.status = 404;
+  }
+};
