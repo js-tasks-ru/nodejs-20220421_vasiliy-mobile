3e019fe5a9201d48a2913bba15bd547be98fab8a
Repkin Vasiliy <vasiliy-mobile@yandex.ru>
2022-06-08 16:50:04 +0200

    Task 06 [02]
    Implement product controller: get products by subcategory

--- 06-mongodb-mongoose/02-rest-api/controllers/products.js
+++ 06-mongodb-mongoose/02-rest-api/controllers/products.js
@@ -1,13 +1,20 @@
+const Product = require('./../models/Product');
+const productMapper = require('./../mappers/product');
+
 module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
   const {subcategory} = ctx.query;
 
   if (!subcategory) return next();
 
-  ctx.body = {};
+  const products = await Product.find({subcategory});
+
+  ctx.body = {products: products.map((product) => productMapper(product))};
 };
 
 module.exports.productList = async function productList(ctx, next) {
-  ctx.body = {};
+  const products = await Product.find();
+
+  ctx.body = {products: products.map((product) => productMapper(product))};
 };
 
 module.exports.productById = async function productById(ctx, next) {
